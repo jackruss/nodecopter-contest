cc32aa34c477a6177fd25f94cdcdc773602ba936
Debbie Rogina <deborah.rogina@gmail.com>
2014-08-08 13:00:09 -0400

    debbie's copter

---
+++ debbie.js
@@ -0,0 +1,52 @@
+re('ar-drone');
+var client = arDrone.createClient();
+
+client.disableEmergency();
+
+client.takeoff();
+
+client
+  .after(1000, function() {
+    this.up(0.5);
+  })
+  .after(0, function() {
+    this.animateLeds('snakeGreenRed', 5, 3000);
+  })
+  .after(0, function() {
+    this.animate('turnaround', 3000);
+  })
+  .after(3000, function() {
+    this.animate('flipLeft', 1000);
+  })
+  .after(1000, function() {
+    this.front(1);
+  })
+  .after(1000, function() {
+    this.animate('wave', 2000);
+  })
+  .after(0, function() {
+    this.animateLeds('blinkGreen', 2000);
+  })
+  .after(5000, function() {
+    this.animate('yawShake', 4000);
+  })
+  .after(0, function() {
+    this.animateLeds('doubleMissile', 4000);
+  })
+  .after(2000, function() {
+    this.up(0.5);
+  })
+  .after(2000, function() {
+    this.animate('turnaroundGodown', 2000);
+  })
+  .after(1000, function() {
+    this.stop();
+    this.land();
+  });
+
+// emergency stop after 30sec
+
+setTimeout(function() {
+  client.stop();
+  client.land();
+}, 30000);
