0e66ed891cd60262c966737c9bd0dfe12b020ba0
Andrew Kennedy <akennedy@carekinesis.com>
2014-08-08 13:23:13 -0400

    update akennedy

--- akennedy.js
+++ akennedy.js
@@ -8,93 +8,46 @@ client.takeoff();
 client
 
   //Go up a little higher
-  .after(5000, function() {
-    this.up(0.3);
-  })
   .after(1000, function() {
-    this.stop();
+    this.up(1);
   })
-
-  //dance and light show
-  .after(5000, function() {
-    this.animate('vzDance', 1000);
-    this.animateLeds('snakeGreenRed', 2, 1)
-  })
-  .after(1000, function() {
+  .after(2000, function() {
     this.stop();
   })
 
 
-  // Go forward
+  //Flip ahead
   .after(5000, function() {
-    this.front(0.5);
+    this.animate('flipAhead', 3000);
   })
-  .after(5000, function() {
+  .after(3000, function() {
     this.stop();
   })
 
 
-  //Dance and light show
+  //Flip right
   .after(5000, function() {
-    this.animate('yawDance', 3000);
-    this.animateLeds('fire', 2, 3)
+    this.animate('flipRight', 3000);
   })
-  .after(5000, function() {
+  .after(3000, function() {
     this.stop();
   })
 
 
-  //Dance and light show
+  //Flip back
   .after(5000, function() {
-    this.animate('thetaDance', 3000);
-    this.animateLeds('redSnake', 2, 3)
+    this.animate('flipBehind', 3000);
   })
-  .after(5000, function() {
+  .after(3000, function() {
     this.stop();
   })
 
 
-  // //Flip ahead
-  // .after(5000, function() {
-  //   this.animate('flipAhead', 3000);
-  // })
-  // .after(3000, function() {
-  //   this.stop();
-  // })
-
-
-  // //Flip right
-  // .after(5000, function() {
-  //   this.animate('flipRight', 3000);
-  // })
-  // .after(3000, function() {
-  //   this.stop();
-  // })
-
-
-  // //Flip back
-  // .after(5000, function() {
-  //   this.animate('flipBehind', 3000);
-  // })
-  // .after(3000, function() {
-  //   this.stop();
-  // })
-
-
-  // //Flip left
-  // .after(5000, function() {
-  //   this.animate('flipLeft', 3000);
-  // })
-  // .after(3000, function() {
-  //   this.stop();
-  // })
-
-
-  //Bo Back
+  //Flip left
   .after(5000, function() {
-    this.back(0.5);
+    this.animate('flipLeft', 3000);
   })
-  .after(5000, function() {
+  .after(3000, function() {
     this.stop();
   })
 
@@ -118,4 +71,4 @@ client
 setTimeout(function() {
   client.stop();
   client.land();
-}, 60000);
+}, 30000);
